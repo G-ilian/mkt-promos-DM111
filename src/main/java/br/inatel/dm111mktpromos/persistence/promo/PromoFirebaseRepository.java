b786486c7c72ac3ebfdc75461a04d1f0364d0822
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-04-22 23:40:44 -0300

    refactor: change some returns and operations with firebase

--- src/main/java/br/inatel/dm111mktpromos/persistence/promo/PromoFirebaseRepository.java
+++ src/main/java/br/inatel/dm111mktpromos/persistence/promo/PromoFirebaseRepository.java
@@ -3,6 +3,8 @@ package br.inatel.dm111mktpromos.persistence.promo;
 import com.google.cloud.firestore.Firestore;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
@@ -25,21 +27,48 @@ public class PromoFirebaseRepository implements PromoRepository{
     }
 
     @Override
-    public Optional<Promo> findAllByPromoId(String promoId) throws ExecutionException, InterruptedException {
-        var spl = firestore.collection(COLLECTION_NAME)
+    public List<Promo> findAllPromos() throws ExecutionException, InterruptedException {
+        List<Promo> promoList = new ArrayList<>();
+
+        var collection = firestore.collection(COLLECTION_NAME);
+
+        var documents = collection.listDocuments();
+
+        for (var documentRef : documents) {
+
+            var document = documentRef.get().get();
+            if (document.exists()) {
+                var promo = document.toObject(Promo.class);
+                promoList.add(promo);
+            }
+        }
+
+        return promoList;
+
+    }
+
+    @Override
+    public Promo findAllByPromoId(String promoId) throws ExecutionException, InterruptedException {
+        var promo = firestore.collection(COLLECTION_NAME)
                 .document(promoId)
                 .get()
                 .get()
                 .toObject(Promo.class);
 
 
-        return Optional.empty();
+        return promo;
     }
 
     @Override
-    public Optional<Promo> findPromoByUserId(String userId) throws ExecutionException, InterruptedException {
+    public Promo findPromoByUserId(String userId) throws ExecutionException, InterruptedException {
+        var promo = firestore.collection(COLLECTION_NAME)
+                .document(userId)
+                .get()
+                .get()
+                .toObject(Promo.class);
 
-        return Optional.empty();
+
+        return promo;
     }
 
     @Override
--- src/main/java/br/inatel/dm111mktpromos/persistence/promo/PromoRepository.java
+++ src/main/java/br/inatel/dm111mktpromos/persistence/promo/PromoRepository.java
@@ -1,15 +1,17 @@
 package br.inatel.dm111mktpromos.persistence.promo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 public interface PromoRepository {
     void save(Promo promo);
 
-    Optional<Promo> findAllByPromoId(String promoId) throws ExecutionException, InterruptedException;
+    List<Promo> findAllPromos() throws ExecutionException, InterruptedException;
+    Promo findAllByPromoId(String promoId) throws ExecutionException, InterruptedException;
 
 
-    Optional<Promo> findPromoByUserId(String userId) throws ExecutionException, InterruptedException;
+    Promo findPromoByUserId(String userId) throws ExecutionException, InterruptedException;
 
     void delete(String promoId) throws ExecutionException, InterruptedException;
 
