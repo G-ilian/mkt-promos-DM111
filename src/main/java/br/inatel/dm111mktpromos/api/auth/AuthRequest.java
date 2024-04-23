44fbb987d469f924d989384d23ae8cdf3c44c7c0
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-04-23 09:15:27 -0300

    feat: authenticate package for user and admin operations

---
+++ src/main/java/br/inatel/dm111mktpromos/api/auth/AuthRequest.java
@@ -0,0 +1,4 @@
+package br.inatel.dm111mktpromos.api.auth;
+
+public record AuthRequest(String email, String password) {
+}
---
+++ src/main/java/br/inatel/dm111mktpromos/api/auth/AuthResponse.java
@@ -0,0 +1,4 @@
+package br.inatel.dm111mktpromos.api.auth;
+
+public record AuthResponse(String token, String userId) {
+}
